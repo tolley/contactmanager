9ececc32160c91484542f0b635dac34ec4dcea35
Chris Tolley <chris.tolley@gmail.com>
2015-12-06 01:22:51 -0500

    Separated the various tasks in my gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var watch = require( 'gulp-watch' );
 var nodemon = require( 'gulp-nodemon' );
 
 // The list of all js files that we care about
-var jsFiles = [
+var clientJSFiles = [
 	'./public/js/angular.js'
 	,'./public/js/angular-route.js'
 	,'./public/js/contact_manager.js'
@@ -24,16 +24,16 @@ var cssFiles = [
 // Process the scss file and creates min.css
 gulp.task( 'sass', function() {
 	return gulp.src( cssFiles)
-			.pipe( sass() )
-			.pipe( concat( 'min.css' ) )
-			.pipe( minifyCSS() )
-			.pipe( gulp.dest( './public/css/' ) );
+		.pipe( sass() )
+		.pipe( concat( 'min.css' ) )
+		.pipe( minifyCSS() )
+		.pipe( gulp.dest( './public/css/' ) );
 } );
 
 
 // Minify our javascript
 gulp.task( 'minjs', function() {
-	return gulp.src( jsFiles )
+	return gulp.src( clientJSFiles )
 		.pipe( minifyJS( {
 			mangle: false
 		} ) )
@@ -51,12 +51,23 @@ gulp.task( 'server-test', function() {
 } );
 
 
-// Set up our watch task to reminify the css and js files
-gulp.task( 'dev', function() {
-	gulp.watch( jsFiles, ['minjs'] );
-	gulp.watch( cssFiles, ['sass'] );
+// Watch the client sided js files for changes and reminify when
+// one is modified
+gulp.task( 'watch-js', function() {
+	return gulp.watch( clientJSFiles, ['minjs'] );
+} );
 
-	nodemon( {
+
+// Watch the css files and reminify when reminify when one is modified
+gulp.task( 'watch-css', function() {
+	return gulp.watch( cssFiles, ['sass'] );
+} );
+
+
+// Watch the server side js files and restart the server when one
+// is modified
+gulp.task( 'watch-server', function() {
+	return nodemon( {
 		script: 'server.js',
 		ext: 'js jade',
 		ignore: [ './public/*', 'gulpfile.js' ]
@@ -65,4 +76,7 @@ gulp.task( 'dev', function() {
 
 
 // Set up our default task
-gulp.task( 'default', ['sass', 'minjs', 'server-test'] );
+gulp.task( 'default', ['sass', 'minjs'] );
+
+// Set up our development task
+gulp.task( 'dev', [ 'watch-js', 'watch-css', 'watch-server' ] );
